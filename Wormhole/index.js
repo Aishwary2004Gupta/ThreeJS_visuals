8bea509e85814b4e97cf4f0d3518ae54831e1af3
Aishwary Gupta <115387744+Aishwary2004Gupta@users.noreply.github.com>
2024-12-02 21:12:18 +0530

    Update index.js

--- Wormhole/index.js
+++ Wormhole/index.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "three"; 
 // import { OrbitControls } from "jsm/controls/OrbitControls.js";
 import spline from "./spline.js";
 import getStarfield from "./star/getStarfield.js";
